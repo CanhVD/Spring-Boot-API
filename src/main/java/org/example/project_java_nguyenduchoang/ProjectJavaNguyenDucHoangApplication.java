642c0ea7d1026ee3fe9641847e3123b224d3f122
CanhVD <canhvuduc@vnpt.vn>
2024-02-20 08:50:55 +0700

    first commit

---
+++ src/main/java/org/example/project_java_nguyenduchoang/ProjectJavaNguyenDucHoangApplication.java
@@ -0,0 +1,13 @@
+package org.example.project_java_nguyenduchoang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaNguyenDucHoangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectJavaNguyenDucHoangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/project_java_nguyenduchoang/ProjectJavaNguyenDucHoangApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.project_java_nguyenduchoang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaNguyenDucHoangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
